5efa64a06f359b9cd0c7073521c836c2ac02b626
Vishak11 <vishakvn007@gmail.com>
2024-03-25 23:49:07 +0530

    v3

--- backEnd/server.js
+++ backEnd/server.js
@@ -7,7 +7,8 @@ const app = express();
 const port = 3000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+//   origin: "http://localhost:5173",
+origin:"*"
 };
 app.use(cors(corsOptions));
 
@@ -15,17 +16,23 @@ app.use(express.json());
 
 const users = [
   {
+    id: 1,
+    name: "vishak",
     email: "vishakvn007@gmail.com",
     password: "password",
     phoneNumber: 9526383363,
   },
   {
+    id: 2,
+    name: "vishak2",
     email: "vishak2@gmail.com",
     password: "password",
     phoneNumber: 9526383362,
   },
   {
-    email: "vishak2@gmail.com",
+    id: 3,
+    name: "vishak3",
+    email: "vishak3@gmail.com",
     password: "password",
     phoneNumber: 9526383362,
   },
@@ -38,42 +45,51 @@ app.post("/submit", (req, res) => {
     email: req.body.email,
     password: req.body.password,
   };
+console.log("data",data);
 
-  if (users.find((user) => user.email === data.email && user.password === data.password)){
+var user=users.find((user) => user.email === data.email && user.password === data.password);
+  if (user){
     isAuthorized = true;
   } else {
     isAuthorized = false;
   }
+  user={...user};
 
-  console.log("email ", data.email);
-  console.log("passWord", data.password);
+
+
   if (!isAuthorized) {
     res.status(401).json({ msg: "invalid credentials" });
     return;
   }
+  delete user.password;
   let jwtSecretKey = process.env.JWT_SECRET_KEY;
   console.log("jwtSecretKey", jwtSecretKey);
-  const token = jwt.sign(data, jwtSecretKey);
+  const token = jwt.sign(user, jwtSecretKey);
   console.log("token", token);
   res.json({ msg: "success", token: token });
 });
 
-app.get("/verify",(req,res)=>{
-    const token = req.headers.authorization;
-    console.log("token",token);
-    if(!token){
+
+app.get("/self-user", (req, res) => {
+const token=req.headers.authorization;
+if(!token){
+    res.status(401).json({msg:"unauthorized"});
+    return
+}
+jwt.verify(token,process.env.JWT_SECRET_KEY,(err,decoded)=>{
+    if(err){
         res.status(401).json({msg:"unauthorized"});
-        return;
+        return
     }
-    jwt.verify(token,process.env.JWT_SECRET_KEY,(err,decoded)=>{
-        if(err){
-        res.status(401).json({msg:"unauthorized"});
-        return;
-        }
-        res.json({msg:"success",decoded:decoded});
-        console.log("newPAge",decoded)
-    });
+    const userId=decoded.id;
+    const user=users.find((users)=>{users.id===userId})
+    console.log("user",user)
+
+
 })
+
+  
+});
 
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
